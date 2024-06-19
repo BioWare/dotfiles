3230d1695cc754b20ce2c67d1a50f6b1ec46b39a
Carlos <kip4akz@gmail.com>
2024-06-19 21:26:13 +0500

    script fix

--- scripts/work.sh
+++ scripts/work.sh
@@ -3,10 +3,13 @@
 # Название сессии tmux
 SESSION_NAME="work_session"
 
-# Пути к файлам
-KANBAN_FILE="~/Sync/obsidian/jesusrag/kanban/kanban.md"
-PLAN_YEAR="~/Sync/obsidian/jesusrag/notes/plan.md"
-PLAN_MONTH="~/Sync/obsidian/jesusrag/plans/May-2024.md"
+# Базовый путь
+BASE_PATH="~/Sync/obsidian/jesusrag"
+
+# Пути к файлам относительно базового пути
+KANBAN_FILE="$BASE_PATH/kanban/kanban.md"
+PLAN_YEAR="$BASE_PATH/notes/plan.md"
+PLAN_MONTH="$BASE_PATH/plans/May-2024.md"
 
 # Проверка, существует ли сессия с таким названием
 if tmux has-session -t $SESSION_NAME 2>/dev/null; then
@@ -19,16 +22,16 @@ fi
 tmux new-session -d -s $SESSION_NAME
 
 # Открытие первого окна с neovim и командой :KanbanOpen
-tmux send-keys "nvim +':KanbanOpen $KANBAN_FILE'" C-m
+tmux send-keys "cd $BASE_PATH && nvim +':KanbanOpen $KANBAN_FILE'" C-m
 
 # Создание второго окна и открытие второго файла в neovim
 tmux split-window -h
-tmux send-keys "nvim $PLAN_YEAR" C-m
+tmux send-keys "cd $BASE_PATH && nvim $PLAN_YEAR" C-m
 
 # Переключение на первую панель и создание третьего окна внизу
 tmux select-pane -t 0
 tmux split-window -v
-tmux send-keys "nvim $PLAN_MONTH" C-m
+tmux send-keys "cd $BASE_PATH && nvim $PLAN_MONTH" C-m
 
 # Установка фокуса на первое окно (верхнее)
 tmux select-pane -t 0
