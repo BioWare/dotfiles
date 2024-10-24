fa9bc6059b6e275856048aff6f807217412878f7
n.abdikov <nurkhan.abdikov@ticketon.kz>
2024-10-24 15:32:29 +0500

    new telescope hotkey

--- neovim/after/plugin/telescope.lua
+++ neovim/after/plugin/telescope.lua
@@ -1,6 +1,17 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>pS', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.live_grep({
+        word_match = "-w",
+        only_sort_text = true,
+        search_dirs = {"."},
+    })
+end)
+
+-- Map <leader>fb to find buffers
+vim.api.nvim_set_keymap('n', '<leader>pb', ':lua require("telescope.builtin").buffers()<CR>', { noremap = true, silent = true })
