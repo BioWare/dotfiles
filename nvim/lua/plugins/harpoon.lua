827d1ec1c32a013277a009576f96c1858156bf0b
n.abdikov <nurkhan.abdikov@ticketon.kz>
2024-12-06 15:49:26 +0500

    fix conflixts with tmux-navigator, neoscroll and harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,19 +6,46 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
+        -- Базовые маппинги harpoon, которые не конфликтуют
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+        -- Функции для установки маппингов harpoon
+        local function setup_harpoon_maps()
+            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+        end
+
+        -- Функция для установки маппингов tmux
+        local function setup_tmux_maps()
+            vim.cmd[[
+                noremap <silent> <c-h> :<C-U>TmuxNavigateLeft<cr>
+                noremap <silent> <c-j> :<C-U>TmuxNavigateDown<cr>
+                noremap <silent> <c-k> :<C-U>TmuxNavigateUp<cr>
+                noremap <silent> <c-l> :<C-U>TmuxNavigateRight<cr>
+            ]]
+        end
+
+        -- Создаем автокоманды для переключения маппингов
+        vim.api.nvim_create_autocmd("WinEnter", {
+            callback = function()
+                if #vim.api.nvim_list_wins() > 1 then
+                    setup_tmux_maps()
+                else
+                    setup_harpoon_maps()
+                end
+            end
+        })
+
+        -- Начальная установка маппингов
+        if #vim.api.nvim_list_wins() > 1 then
+            setup_tmux_maps()
+        else
+            setup_harpoon_maps()
+        end
+
+
     end,
-    keys = {
-        "<leader>a",
-        "<C-e>",
-        "<C-h>",
-        "<C-t>",
-        "<C-n>",
-        "<C-s>"
-    },
 }
